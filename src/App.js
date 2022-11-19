5409bd3499a40284801dc0c9ae8eb3b62c6922c6
abenteuerzeit <amroz.muzyka@gmail.com>
2022-11-19 15:52:10 +0100

    binding exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value : ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert(`A name was submitted: ${this.state.value}`)
+    event.preventDefault();
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>{this.state.value}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Text Area:
+            <textarea name="textarea" value={this.state.value} onChange={this.handleChange} placeholder="Enter a value" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
+  }
+
 }
 
-export default App;
+export default App
